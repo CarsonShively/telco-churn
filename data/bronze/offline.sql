5995d9422f758626e2659f8df0f75c04334e84e5
Carson Shively <carsonshively24@gmail.com>
2025-12-12 10:28:24 -0700

    Create bronze data layer folder and offline bronze sql

---
+++ data/bronze/offline.sql
@@ -0,0 +1,10 @@
+INSTALL httpfs;
+LOAD httpfs;
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+
+CREATE OR REPLACE TABLE bronze.offline AS
+SELECT *
+FROM read_parquet(
+  'https://huggingface.co/datasets/Carson-Shively/telco-churn/resolve/main/data/bronze/offline.parquet'
+);
