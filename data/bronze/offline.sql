308b325dfb77195651015f7c264eb6b4b9376780
Carson Shively <carsonshively24@gmail.com>
2025-12-12 12:05:57 -0700

    convert bronze sql from table to view

--- data/bronze/offline.sql
+++ data/bronze/offline.sql
@@ -1,9 +1,8 @@
-INSTALL httpfs;
 LOAD httpfs;
 
 CREATE SCHEMA IF NOT EXISTS bronze;
 
-CREATE OR REPLACE TABLE bronze.offline AS
+CREATE OR REPLACE VIEW bronze.offline AS
 SELECT *
 FROM read_parquet(
   'https://huggingface.co/datasets/Carson-Shively/telco-churn/resolve/main/data/bronze/offline.parquet'
