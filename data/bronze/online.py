35776506c3bbee9fbfe822a8c368f35a520f13df
Carson Shively <carsonshively24@gmail.com>
2025-12-12 12:33:36 -0700

    update brzone online dataframe read

--- data/bronze/online.py
+++ data/bronze/online.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 URL = "https://huggingface.co/datasets/Carson-Shively/telco-churn/resolve/main/data/bronze/online_bronze.parquet"
 
-bronze_py = pd.read_parquet(URL, engine="pyarrow")
+df = pd.read_parquet(URL, engine="pyarrow")
