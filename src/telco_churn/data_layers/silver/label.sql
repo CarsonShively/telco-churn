3066f3ef5e9bc8320f68ac421a58bb1845d49d8e
Carson Shively <carsonshively24@gmail.com>
2025-12-14 10:06:13 -0700

    swap silver layer to base + labels sql setup

---
+++ src/telco_churn/data_layers/silver/label.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS silver;
+
+CREATE OR REPLACE TABLE silver.labels AS
+SELECT
+  lower(trim(customerID)) AS customer_id,
+  CASE
+    WHEN lower(trim(Churn)) = 'yes' THEN 1
+    WHEN lower(trim(Churn)) = 'no'  THEN 0
+    ELSE NULL
+  END AS churn
+FROM bronze.offline;
