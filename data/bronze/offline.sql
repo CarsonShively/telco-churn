8ba4767c0cb2b0594bc75b4f5e1a5af54bb38d69
Carson Shively <carsonshively24@gmail.com>
2025-12-14 08:15:14 -0700

    refractored obsoliete folder

--- data/bronze/offline.sql
+++
@@ -1,9 +0,0 @@
-LOAD httpfs;
-
-CREATE SCHEMA IF NOT EXISTS bronze;
-
-CREATE OR REPLACE VIEW bronze.offline AS
-SELECT *
-FROM read_parquet(
-  'https://huggingface.co/datasets/Carson-Shively/telco-churn/resolve/main/data/bronze/offline.parquet'
-);
--- data/bronze/online.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-URL = "https://huggingface.co/datasets/Carson-Shively/telco-churn/resolve/main/data/bronze/online_bronze.parquet"
-
-df = pd.read_parquet(URL, engine="pyarrow")
